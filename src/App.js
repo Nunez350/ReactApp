0cd8df08250d00e66846bfd2869c4e0159db073a
Roy Nunez <roynunez273@gmail.com>
2019-11-05 19:00:24 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Header from "./components/Header"
+import MainContent from "./components/MainContent"
+import Footer from "./components/Footer"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+return (
+    <div>
+
+        <div>
+            <input type="checkbox"/>
+            <p>Placeholder text here</p>
+            <input type="checkbox"/>
+            <p>Placeholder text here</p>
+            <input type="checkbox"/>
+            <p>Placeholder text here</p>
+
+        </div>
+        <Header/>
+        <MainContent/>
+        <Footer/>
+
+
     </div>
-  );
+
+    
+    )    
 }
 
-export default App;
+export default App
+
+
+
+
+
+
+
+
+
+    // <div>
+    //     <input type = "checkbox"/>
+    //     <p>Place holder </p>
+
+    //     <input type = "checkbox"/>
+    //     <p>Place holder </p>
+        
+    //     <input type = "checkbox"/>
+    //     <p>Place holder </p>
+
+    //     <input type = "checkbox"/>
+    //     <p>Place holder </p>
+
+    //     <input type = "checkbox"/>
+    //     <p>Place holder </p>
+    // </div>
+
+
+// import Header from "./Header"
+// import MainContent from "./MainContent"
+// import Footer from "./Footer"
+
+
+
+// function App(){
+//     return (
+//         <div>
+//             <Header/>
+//             <MainContent>aaaa</MainContent>
+//             <nav>
+//             <h1> hello a third time!</h1>
+//         <h1> hello again</h1>
+//         <ul>
+//             <li>thing1 </li>
+//         </ul>
+//         </nav>
+//         <main>
+//             <p> this is where most of </p>
+//         </main>
+//         <Footer/>
+//         </div>
+//     )
+// }
+// export default App
