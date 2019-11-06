cb1b16f692471d88d42a3b1a315c4278ad0982e1
Roy Nunez <roynunez273@gmail.com>
2019-11-06 17:36:48 -0500

    woprking to do app

--- src/App.js
+++ src/App.js
@@ -2,39 +2,76 @@ import React from "react"
 import TodoItem from "./components/TodoItem"
 import todosData from "./components/todosData"
 
-
 class App extends React.Component {
-    constructor() {
+    constructor (){
         super()
         this.state = {
-           count: 0
+            todos: todosData
         }
-        this.handleClick = this.handleClick.bind(this)
-    }
-handleClick() {
-  this.setState(prevState => {
-    return {
-        count: prevState.count + 1
+        this.handleChange = this.handleChange.bind(this)
 
     }
-  })
-   
-
-}
+    handleChange(id){
+        this.setState(prevState => {
+        const updatedTodos = prevState.todos.map(todo => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed
+            }
+            return todo
+        })
+        return {
+            todos:  updatedTodos
+        }
+        })
+    }
     render() {
-       
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}
+        handleChange={this.handleChange}/>)
         return (
-            <div>
-            <h1>{this.state.count}</h1>
-            <button onClick ={this.handleClick}>Change!</button>
+            <div classname = "todo-list">
+                {todoItems}
 
-
-           
-                </div>
+            </div>
         )
     }
 }
+
 export default App
+
+
+//updating states
+// class App extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//            count: 0
+//         }
+//         this.handleClick = this.handleClick.bind(this)
+//     }
+// handleClick() {
+//   this.setState(prevState => {
+//     return {
+//         count: prevState.count + 1
+
+//     }
+//   })
+   
+
+// }
+//     render() {
+       
+//         return (
+//             <div>
+//             <h1>{this.state.count}</h1>
+//             <button onClick ={this.handleClick}>Change!</button>
+
+
+           
+//                 </div>
+//         )
+//     }
+// }
+// export default App
 
 // function App() {
 //     return (
